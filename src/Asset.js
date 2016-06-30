e4f1d63ca135a89a122d73c4ef9289693550834c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-06-30 05:34:11 +0000

    Load assets from dev server when dev'ing
    fbshipit-source-id: e194e73

--- src/Asset.js
+++ src/Asset.js
@@ -1,21 +1,41 @@
+'use strict';
+
+import {
+  Platform
+} from 'react-native';
+
 import AssetRegistry from 'AssetRegistry';
 import AssetSourceResolver from 'AssetSourceResolver';
 import PixelRatio from 'PixelRatio';
 import resolveAssetSource from 'resolveAssetSource';
 
-// Construct the CDN uri for an asset, picking the correct scale if it's an
-// image
+import { manifest } from './Constants';
+
+// Construct the CDN URI for an asset from its React Native metadata, picking
+// the correct scale if it's an image
 const getURI = (asset) => {
-  const scale = AssetSourceResolver.pickScale(asset.scales, PixelRatio.get());
+  const scale = asset.scales.length > 1 ?
+                AssetSourceResolver.pickScale(asset.scales, PixelRatio.get()) :
+                1;
+
+  if (manifest.xde) {
+    // Development server URI is pieced together
+    const suffix = scale === 1 ? '' : '@' + scale + 'x';
+    return manifest.bundleUrl.match(/^https?:\/\/.*?\//)[0] +
+           asset.httpServerLocation.replace(/^\/?/, '') +
+           '/' + asset.name + suffix + '.' + asset.type +
+           '?platform=' + Platform.OS + '&hash=' + asset.hash;
+  }
+
+  // CDN URI is based directly on the hash
   const index = asset.scales.findIndex(s => s === scale);
   return 'https://d1wp6m56sqw74a.cloudfront.net/~assets/' +
          (asset.fileHashes[index] || asset.fileHashes[0]);
 };
 
 // Override react-natives asset resolution for `Image` components
-resolveAssetSource.setCustomSourceTransformer((resolver) => {
-  return resolver.fromSource(getURI(resolver.asset));
-});
+resolveAssetSource.setCustomSourceTransformer((resolver) =>
+  resolver.fromSource(getURI(resolver.asset)));
 
 export function fromRequire(id) {
   const asset = AssetRegistry.getAssetByID(id);
