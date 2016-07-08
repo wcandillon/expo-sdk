dca1840b6dbe1e5e342d34d35ce937e709693b10
Brent Vatne <brentvatne@gmail.com>
2016-07-08 19:35:00 +0000

    Create lib and change template to use it
    Summary: Create wrapper around react-native-vector-icons called exponent-vector-icons and use it by default in new-project template
    
    Test Plan: Open new project template, see that icons in tab bar work
    
    Reviewers: ide, nikki
    
    Subscribers: skevy
    
    Differential Revision: D2996
    
    fbshipit-source-id: b4c9365

--- src/Font.js
+++ src/Font.js
@@ -8,6 +8,8 @@ import { fromRequire } from './Asset';
 
 const sessionId = NativeModules.ExponentConstants.sessionId;
 const loaded = {};
+const loading = {};
+const onLoadPromises = {};
 
 function nativeName(name) {
   return `${sessionId}-${name}`;
@@ -15,25 +17,47 @@ function nativeName(name) {
 
 type FontUriMap = { [name: string]: string };
 
-export async function loadAsync(nameOrMap: string & FontUriMap, uriOrAssetModule: string & number) {
+export function isLoaded(name:string) {
+  return !!loaded[name];
+}
+
+export async function loadAsync(nameOrMap: string & FontUriMap, uriOrAssetModule: string | number) {
   if (typeof nameOrMap === 'object') {
     const names = Object.keys(nameOrMap);
     await Promise.all(names.map(name => loadAsync(name, nameOrMap[name])));
     return;
   }
 
-  let uri;
-  if (typeof uriOrAssetModule === 'string') {
-    uri = uriOrAssetModule;
+  let name = nameOrMap;
+
+  if (loaded[name]) {
+    return;
+  } else if (loading[name]) {
+    await new Promise(resolve => { onLoadPromises[name].push(resolve); });
   } else {
-    uri = fromRequire(uriOrAssetModule).uri;
+    loading[name] = true;
+    onLoadPromises[name] = [];
+
+    let uri;
+    if (typeof uriOrAssetModule === 'string') {
+      uri = uriOrAssetModule;
+    } else {
+      uri = fromRequire(uriOrAssetModule).uri;
+    }
+
+    await NativeModules.ExponentFontLoader.loadAsync(nativeName(name), uri);
+    loaded[name] = true;
+    delete loading[name];
+
+    if (onLoadPromises[name]) {
+      onLoadPromises[name].forEach(resolve => resolve());
+      delete onLoadPromises[name];
+    }
   }
-  await NativeModules.ExponentFontLoader.loadAsync(nativeName(nameOrMap), uri);
-  loaded[nameOrMap] = true;
 }
 
-export function style(name: string) {
-  if (!loaded[name]) {
+export function style(name: string, options:{ignoreWarning: bool} = {ignoreWarning: false}) {
+  if (!loaded[name] && !options.ignoreWarning) {
     console.warn(`[Exponent.Font] No font '${name}', or it hasn't been loaded yet`);
   }
   return {
