678bbb0d16e34b47c820c4b481b33107eed6f562
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-07-26 22:50:04 +0000

    iOS: `AppLoading` shows "Loading..." for now
    fbshipit-source-id: 2b4a834

--- src/Components/AppLoading.ios.js
+++ src/Components/AppLoading.ios.js
@@ -1,3 +1,19 @@
 'use strict';
 
-export default () => null;
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default () => (
+  <View
+    style={{ flex: 1,
+             justifyContent: 'center',
+             alignItems: 'center',
+             backgroundColor: '#fff' }}>
+    <Text style={{ fontSize: 14 }}>
+      Loading...
+    </Text>
+  </View>
+);
