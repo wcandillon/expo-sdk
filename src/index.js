5ddf917c8f8b0e4907dc6163840a79f59514f38b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-08-09 23:16:41 +0000

    Ultimate fix that makes every variation of import work
    fbshipit-source-id: ccd740d

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Exponent from './Exponent';
+export * from './Exponent';
 
+import * as Exponent from './Exponent';
+export default Exponent;
 if (global) {
   global.__exponent = Exponent;
 }
-
-export default Exponent;
