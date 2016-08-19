7c4d36d8cfa55977bb60abf3f79d0aea80f8dddb
Janic Duplessis <janicduplessis@gmail.com>
2016-08-19 21:11:26 +0000

    Export react-native-svg modules from exponent
    fbshipit-source-id: 6ca197e

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,3 +5,11 @@ export BlurView from './BlurView';
 export LinearGradient from './LinearGradient';
 export Video from './Video';
 
+import * as SvgModules from 'react-native-svg';
+let { Svg } = SvgModules;
+for (let key in SvgModules) {
+  if (key !== 'default' && key !== 'Svg') {
+    Svg[key] = SvgModules[key];
+  }
+}
+export { Svg };
