d15e1e344d644ef43779684d795ceecddbd88242
Brent Vatne <brentvatne@gmail.com>
2016-08-19 03:04:01 +0000

    Fix event emitter to use the new NativeEventEmitter class
    Summary:
    The iOS event system changed between SDK7 and SDK8 and now requires us to use NativeEventEmitter as shown in this diff.
    Also see https://github.com/facebook/react-native/blob/master/Libraries/Geolocation/Geolocation.js for an example of it in use.
    
    Test Plan: Open up native-component-list/__internal__ and try out location on iOS and Android (or don't, it works).
    
    Reviewers: nikki, ide
    
    Reviewed By: ide
    
    Subscribers: ben
    
    Differential Revision: D3023
    
    fbshipit-source-id: 43ce8bf

--- src/Location.js
+++ src/Location.js
@@ -1,7 +1,7 @@
 // @flow
 
 import {
-  DeviceEventEmitter,
+  NativeEventEmitter,
   NativeModules,
 } from 'react-native';
 
@@ -24,6 +24,8 @@ type LocationData = {
 }
 
 type LocationCallback = (data: LocationData) => any;
+
+const LocationEventEmitter = new NativeEventEmitter(NativeModules.ExponentLocation);
 
 let nextWatchId = 0;
 let watchCallbacks: { [watchId: number]: LocationCallback } = {};
@@ -49,15 +51,17 @@ export function getCurrentPositionAsync(options: LocationOptions) {
 }
 
 export async function watchPositionAsync(options: LocationOptions, callback: LocationCallback) {
+  let { ExponentLocation } = NativeModules;
+
   if (!deviceEventSubscription) {
-    deviceEventSubscription = DeviceEventEmitter.addListener(
+    deviceEventSubscription = LocationEventEmitter.addListener(
       'Exponent.locationChanged',
       ({ watchId, location }) => {
         const callback = watchCallbacks[watchId];
         if (callback) {
           callback(location);
         } else {
-          NativeModules.ExponentLocation.removeWatchAsync(watchId);
+          ExponentLocation.removeWatchAsync(watchId);
         }
       },
     );
@@ -65,14 +69,17 @@ export async function watchPositionAsync(options: LocationOptions, callback: Loc
 
   const watchId = nextWatchId++; // XXX: thread safe?
   watchCallbacks[watchId] = callback;
-  await NativeModules.ExponentLocation.watchPositionImplAsync(watchId, options);
+  await ExponentLocation.watchPositionImplAsync(watchId, options);
 
   let removed = false;
   return {
     remove() {
       if (!removed) {
-        NativeModules.ExponentLocation.removeWatchAsync(watchId);
+        ExponentLocation.removeWatchAsync(watchId);
         delete watchCallbacks[watchId];
+        if (Object.keys(watchCallbacks).length === 0) {
+          LocationEventEmitter.removeSubscription(deviceEventSubscription);
+        }
         removed = true;
       }
     },
