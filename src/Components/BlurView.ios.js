0d94b72cc28af90114d0b0a91891b135da3d94d9
Brent Vatne <brentvatne@gmail.com>
2016-11-21 21:25:10 +0000

    Add BlurView intensity prop and rename tintEffect to tint
    fbshipit-source-id: 5be9ea5

--- src/Components/BlurView.ios.js
+++ src/Components/BlurView.ios.js
@@ -10,20 +10,35 @@ import {
   requireNativeComponent,
  } from 'react-native';
 
+import deprecatedPropType from 'react-native/Libraries/Utilities/deprecatedPropType';
+
 export default class BlurView extends Component {
   static propTypes = {
-    tintEffect: PropTypes.oneOf(['light', 'default', 'dark']).isRequired,
+    tintEffect: deprecatedPropType(
+      PropTypes.string,
+      'Use the `tint` prop instead.'
+    ),
+    tint: PropTypes.oneOf(['light', 'default', 'dark']).isRequired,
+    intensity: PropTypes.number.isRequired,
     ...View.propTypes,
   };
 
   static defaultProps = {
-    tintEffect: 'default',
+    tint: 'default',
+    intensity: 50,
   }
 
   render() {
+    let { style, ...nativeProps } = this.props;
+
+    if (nativeProps.tintEffect) {
+      nativeProps.tint = nativeProps.tintEffect;
+      delete nativeProps.tintEffect;
+    }
+
     return (
       <NativeBlurView
-        {...this.props}
+        {...nativeProps}
         style={[this.props.style, { backgroundColor: 'transparent' }]}
       />
     );
