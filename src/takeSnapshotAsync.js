011c1a742f287b86a5c59a7e4077ea639543d61c
Brent Vatne <brentvatne@gmail.com>
2016-11-30 21:48:58 +0000

    @brentvatne/update third party deps (#43)
    * [exponent-sdk] Fix takeSnapshotAsync for flow
    
    * [exponent-sdk, ios, android, native-component-list] Update react-native-svg to 4.3.3
    
    * [vector-icons] Fix typo in README
    
    fbshipit-source-id: d20635c

--- src/takeSnapshotAsync.js
+++ src/takeSnapshotAsync.js
@@ -7,19 +7,17 @@ const { RNViewShot } = NativeModules;
 export default async function takeSnapshotAsync(
   view: ?(number | ReactElement<any>),
   options ?: {
-    width ?: number,
-    height ?: number,
-    format ?: "png" | "jpg" | "jpeg" | "webm",
-    quality ?: number,
-    base64 ?: bool,
+    width ?: number;
+    height ?: number;
+    format ?: "png" | "jpg" | "jpeg" | "webm";
+    quality ?: number;
+    result ?: "file" | "base64" | "data-uri";
   }
 ): Promise<string> {
-  if (typeof view !== "number" && view) {
-    view = findNodeHandle(view);
-  }
-
-  if (!view) {
-    return Promise.reject();
+  if (typeof view !== "number") {
+    const node = findNodeHandle(view);
+    if (!node) return Promise.reject(new Error("findNodeHandle failed to resolve view="+String(view)));
+    view = node;
   }
 
    return RNViewShot.takeSnapshot(view, options);
