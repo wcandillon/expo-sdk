10895a66f303704cd1ee0c1298af4d9c277d8bd4
Greg Kufera <greg.kufera@gmail.com>
2017-02-15 17:43:03 +0000

    Adding rate to the audio API.
    fbshipit-source-id: fa6ef96

--- src/Audio.js
+++ src/Audio.js
@@ -5,35 +5,64 @@ import {
 } from 'react-native';
 import Asset from './Asset';
 
-export async function setIsEnabled(value) {
-  return await NativeModules.ExponentAudio.setIsEnabled(value);
+type SoundStatus = {
+  isPlaying: boolean,
+  positionMillis: number,
+  rate: number,
+  shouldCorrectPitch: boolean,
+  volume: number,
+  isMuted: boolean,
+  isLooping: boolean,
+}
+
+var _enabled: boolean = false;
+const _DISABLED_ERROR: Error = new Error('Cannot complete operation because audio is not enabled.');
+const _NOT_LOADED_ERROR: Error = new Error('Cannot complete operation because sound is not loaded.');
+const _DEFAULT_POLLING_TIMEOUT_MILLIS: number = 500;
+
+export async function setIsEnabledAsync(value: boolean): Promise<void> {
+  _enabled = value;
+  await NativeModules.ExponentAudio.setIsEnabled(value);
 }
 
 export class Sound {
-  constructor({ source }) {
+  uri: string;
+  loaded: boolean;
+  key: number;
+  durationMillis: number;
+  statusChangeCallback: ?(status: SoundStatus) => void;
+  userPlaybackFinishedCallback: ?() => void;
+  statusPollingTimeoutVariable: ?number;
+  statusPollingTimeoutMillis: number;
+
+  constructor({ source } : { source: number | string | Asset }) {
     if (typeof source === 'number') { // source is an asset module
       let asset = Asset.fromModule(source);
       this.uri = asset.localUri || asset.uri;
-    } else if (source.constructor === Asset) {
-      this.uri = source.localUri || source.uri;
-    } else { // source is a remote URI
+    } else if (typeof source === 'string') { // source is a remote URI
       this.uri = source;
+    } else { // source is an Asset
+      this.uri = source.localUri || source.uri;
     }
 
     this.loaded = false;
     this.key = -1;
+    this.durationMillis = 0;
     this.statusChangeCallback = null;
     this.userPlaybackFinishedCallback = null;
     this.statusPollingTimeoutVariable = null;
-    this.statusPollingTimeoutMillis = 100;
+    this.statusPollingTimeoutMillis = _DEFAULT_POLLING_TIMEOUT_MILLIS;
   }
 
   _statusPollingLoop = () => {
+    if (!_enabled) {
+      return;
+    }
     if (this.statusChangeCallback != null) {
-      this.getStatus(); // Automatically calls this.statusChangeCallback.
+      this.getStatusAsync(); // Automatically calls this.statusChangeCallback.
     }
     if (this.loaded) {
-      this.statusPollingTimeoutVariable = setTimeout(this._statusPollingLoop, this.pollingTimeoutMillis);
+      this.statusPollingTimeoutVariable = setTimeout(this._statusPollingLoop, this.statusPollingTimeoutMillis);
     }
   }
 
@@ -45,17 +74,19 @@ export class Sound {
   }
 
   _enableStatusPolling() {
-    this._disableStatusPolling();
-    this._statusPollingLoop();
+    if (_enabled) {
+      this._disableStatusPolling();
+      this._statusPollingLoop();
+    }
   }
 
-  _tryCallStatusChangeCallbackForStatus(status) {
+  _tryCallStatusChangeCallbackForStatus(status: SoundStatus) {
     if (this.statusChangeCallback != null) {
       this.statusChangeCallback(status);
     }
   }
 
-  _internalPlaybackFinishedCallback = (status) => {
+  _internalPlaybackFinishedCallback = (status: SoundStatus) => {
     this._tryCallStatusChangeCallbackForStatus(status);
     if (this.userPlaybackFinishedCallback != null) {
       this.userPlaybackFinishedCallback();
@@ -69,39 +100,45 @@ export class Sound {
     }
   }
 
-  async _performOperationAndUpdateStatus(operation) {
+  async _performOperationAndUpdateStatusAsync(operation: () => Promise<{ status: SoundStatus }>): Promise<SoundStatus> {
+    if (!_enabled) {
+      throw _DISABLED_ERROR;
+    }
     if (this.loaded) {
-      const { status } = await operation();
+      const { status } : { status: SoundStatus } = await operation();
       this._tryCallStatusChangeCallbackForStatus(status);
       return status;
+    } else {
+      throw _NOT_LOADED_ERROR;
     }
-    return null;
   }
 
-  async loadAsync() {
+  async loadAsync(): Promise<void> {
+    if (!_enabled) {
+      throw _DISABLED_ERROR;
+    }
     if (!this.loaded) {
-      try {
-        const result = await NativeModules.ExponentAudio.load(this.uri);
-        this.key = result.key;
-        this.durationMillis = result.duration_millis;
-        this.loaded = true;
-        this._setInternalPlaybackFinishedCallback();
-      } catch (error) {
-        // Sound not created!
-      }
+      const result: {
+        key: number,
+        durationMillis: number,
+        status: SoundStatus
+      } = await NativeModules.ExponentAudio.load(this.uri);
+      this.key = result.key;
+      this.durationMillis = result.durationMillis;
+      this.loaded = true;
+      this._setInternalPlaybackFinishedCallback();
     }
-    return this;
   }
 
-  isLoaded() {
+  isLoaded(): boolean {
     return this.loaded;
   }
 
-  getDurationMillis() {
-    return this.durationMillis;
+  getDurationMillis(): ?number {
+    return this.loaded ? this.durationMillis : null;
   }
 
-  setStatusChangeCallback(callback) {
+  setStatusChangeCallback(callback: ?(status: SoundStatus) => void) {
     this.statusChangeCallback = callback;
     if (callback == null) {
       this._disableStatusPolling();
@@ -110,53 +147,72 @@ export class Sound {
     }
   }
 
-  setPlaybackFinishedCallback(callback) {
+  setPlaybackFinishedCallback(callback: ?() => void) {
     this.userPlaybackFinishedCallback = callback;
   }
 
-  setStatusPollingTimeoutMillis(value) {
+  setStatusPollingTimeoutMillis(value: number) {
     this.statusPollingTimeoutMillis = value;
   }
 
-  async unload() {
+  async unloadAsync(): Promise<void> {
     if (this.loaded) {
       this.loaded = false;
       this._disableStatusPolling();
       this.userPlaybackFinishedCallback = null;
       this.statusChangeCallback = null;
-      await NativeModules.ExponentAudio.unload(this.key);
+      return await NativeModules.ExponentAudio.unload(this.key);
     }
   }
 
-  async play() {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.play(this.key));
+  async playAsync(): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.play(this.key));
   }
 
-  async pause() {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.pause(this.key));
+  async pauseAsync(): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.pause(this.key));
   }
 
-  async stop() {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.stop(this.key));
+  async stopAsync(): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.stop(this.key));
   }
 
-  async setPosition(millis) {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.setPosition(this.key, millis));
+  async setPositionAsync(millis: number): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.setPosition(this.key, millis));
   }
 
-  async setVolume(value) {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.setVolume(this.key, value));
+  async setRateAsync(value: number, shouldCorrectPitch: boolean): Promise<SoundStatus> {
+    if (value < 0.0 || value > 32.0) {
+      throw new Error('Rate value must be between 0.0 and 32.0.');
+    }
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.setRate(this.key, value, shouldCorrectPitch));
   }
 
-  async setIsMuted(value) {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.setIsMuted(this.key, value));
+  async setVolumeAsync(value: number): Promise<SoundStatus> {
+    if (value < 0.0 || value > 1.0) {
+      throw new Error('Volume value must be between 0.0 and 1.0.');
+    }
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.setVolume(this.key, value));
   }
 
-  async setIsLooping(value) {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.setIsLooping(this.key, value));
+  async setIsMutedAsync(value: boolean): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.setIsMuted(this.key, value));
   }
 
-  async getStatus() {
-    return this._performOperationAndUpdateStatus(() => NativeModules.ExponentAudio.getStatus(this.key));
+  async setIsLoopingAsync(value: boolean): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.setIsLooping(this.key, value));
+  }
+
+  async getStatusAsync(): Promise<SoundStatus> {
+    return this._performOperationAndUpdateStatusAsync(
+      () => NativeModules.ExponentAudio.getStatus(this.key));
   }
 }
