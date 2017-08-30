dd63976050131e93ddb46b7f6be39d028abefa45
James Ide <ide@jameside.com>
2017-08-30 21:11:23 +0000

    Stub out Payments.initialize with warning
    fbshipit-source-id: c9be0bc

--- src/Payments.js
+++ src/Payments.js
@@ -1,3 +1,5 @@
-import { NativeModules } from 'react-native';
-
-module.exports = NativeModules.TPSStripeManager;
+exports.initialize = function() {
+  console.warn(
+    `We temporarily removed the Expo Payments API. Please see the SDK 20 release notes for more information: https://blog.expo.io/expo-sdk-v20-0-0-is-now-available-79f84232a9d1`
+  );
+};
