31efc2d4e70a549516a14303a72706f56bcb8938
Jeffrey Da <jzda@uw.edu>
2017-09-13 00:02:55 +0000

    Move Payments module to ExpoKit (#961)
    * Change payments.js to detect if Payments module is implemented
    
    * Add docs for ExpoKit payments module (Part 1)
    
    * [expo-sdk] Update to check for Payments module
    
    * [expo-sdk] Have Payments module if app is detached
    
    * [docs] Change comments to ruby comments
    
    * [ios] Move Payments and remote ExpoPayments.podspec
    
    * [ios] Remove payments from ExpoKit Core
    
    * [expo-sdk] Fix Payments warning
    
    * [ios] Move to UnversionedModules:
    
    * Fix changes
    
    fbshipit-source-id: ccf078d

--- src/Payments.js
+++ src/Payments.js
@@ -1,5 +1,13 @@
+import { NativeModules } from 'react-native';
+
 exports.initialize = function() {
-  console.warn(
-    `We temporarily removed the Expo Payments API. Please see the SDK 20 release notes for more information: https://blog.expo.io/expo-sdk-v20-0-0-is-now-available-79f84232a9d1`
-  );
+  if (!NativeModules.TPSStripeManager) {
+    console.warn(
+      `We temporarily moved the Expo Payments API to ExpoKit. Please see the SDK 20 release notes for more information: https://blog.expo.io/expo-sdk-v20-0-0-is-now-available-79f84232a9d1`
+    );
+  }
 };
+
+if (NativeModules.TPSStripeManager) {
+  module.exports = NativeModules.TPSStripeManager;
+}
