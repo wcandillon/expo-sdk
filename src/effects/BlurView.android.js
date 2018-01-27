7ca949dd8bfb4eeeb500e9305e168b4f7b6baa22
William Candillon <wcandillon@gmail.com>
2018-01-27 19:27:46 +0100

    Update BlurView.android.js

--- src/effects/BlurView.android.js
+++ src/effects/BlurView.android.js
@@ -1,31 +1,30 @@
 // @flow
+import * as React from "react";
+import { View, Animated } from "react-native";
 
-import PropTypes from 'prop-types';
-import * as React from 'react';
-import { View, ViewPropTypes } from 'react-native';
-
-type Props = {
-  tint: 'light' | 'dark' | 'default',
+type BlurViewProps = {
+  intensity: number | Animated.Value,
+  tint: "light" | "dark" | "default"
 } & React.ElementProps<typeof View>;
 
-export default class BlurView extends React.Component<Props> {
-  static propTypes = {
-    tint: PropTypes.oneOf(['light', 'default', 'dark']),
-    ...ViewPropTypes,
+export default class BlurView extends React.PureComponent<BlurViewProps> {
+
+  static defaultProps = {
+      intensity: 100
   };
 
-  render() {
-    let { tint, ...props } = this.props;
-
+  render(): React.Node {
+    const { tint, intensity, ...props } = this.props;
+    let opacity = 1;
     let backgroundColor;
-    if (tint === 'dark') {
-      backgroundColor = 'rgba(0,0,0,0.5)';
-    } else if (tint === 'light') {
-      backgroundColor = 'rgba(255,255,255,0.7)';
+    if (tint === "dark") {
+      backgroundColor = `rgba(0, 0, 0, ${(100 / 0.5) * intensity})`;
+    } else if (tint === "light") {
+      backgroundColor = `rgba(255, 255, 255, ${(100 / 0.7) * intensity})`;
     } else {
-      backgroundColor = 'rgba(255,255,255,0.4)';
+      backgroundColor = `rgba(255,255,255, ${(100 / 0.4) * intensity})`;
     }
 
-    return <View {...props} style={[this.props.style, { backgroundColor }]} />;
+    return <Animated.View {...props} style={[this.props.style, { backgroundColor, opacity }]} />;
   }
 }
